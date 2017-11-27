cf154159769333fca37bb0b9e0fafe5c23d316a5
LearnerDeOuf13 <etienne.pochet@hotmail.fr>
2017-11-27 01:37:50 +0100

    improved

--- predict_xgboost.py
+++ predict_xgboost.py
@@ -72,17 +72,54 @@ def predict_xgboost_k_fold(data,k,test_v):
     
     return np.mean(results),pred/k
 
+def test(train_df, test_df) :
+    k=5
+    kf=KFold(n_splits=k)
+    results=0
+    #feature_columns_to_use = ['ps_ind_01','ps_ind_02_cat','ps_ind_03','ps_ind_04_cat','ps_ind_05_cat','ps_ind_06_bin','ps_ind_07_bin','ps_ind_08_bin','ps_ind_09_bin','ps_ind_10_bin','ps_ind_11_bin','ps_ind_12_bin','ps_ind_13_bin','ps_ind_14','ps_ind_15','ps_ind_16_bin','ps_ind_17_bin','ps_ind_18_bin','ps_reg_01','ps_reg_02','ps_reg_03','ps_car_01_cat','ps_car_02_cat','ps_car_03_cat','ps_car_04_cat','ps_car_05_cat','ps_car_06_cat','ps_car_07_cat','ps_car_08_cat','ps_car_09_cat','ps_car_10_cat','ps_car_11_cat','ps_car_11','ps_car_12','ps_car_13','ps_car_14','ps_car_15','ps_calc_01','ps_calc_02','ps_calc_03','ps_calc_04','ps_calc_05','ps_calc_06','ps_calc_07','ps_calc_08','ps_calc_09','ps_calc_10','ps_calc_11','ps_calc_12','ps_calc_13','ps_calc_14','ps_calc_15_bin','ps_calc_16_bin','ps_calc_17_bin','ps_calc_18_bin','ps_calc_19_bin','ps_calc_20_bin']
+    feature_columns_to_use = ['ps_ind_01', 'ps_ind_02_cat', 'ps_ind_03', 'ps_ind_04_cat', 'ps_ind_05_cat', 'ps_ind_06_bin', 'ps_ind_07_bin', 'ps_ind_08_bin', 'ps_ind_09_bin', 'ps_ind_14', 'ps_ind_15', 'ps_ind_16_bin', 'ps_ind_17_bin', 'ps_ind_18_bin', 'ps_reg_01', 'ps_reg_02', 'ps_reg_03', 'ps_car_01_cat', 'ps_car_02_cat', 'ps_car_03_cat', 'ps_car_04_cat', 'ps_car_05_cat', 'ps_car_06_cat', 'ps_car_07_cat', 'ps_car_08_cat', 'ps_car_09_cat', 'ps_car_10_cat', 'ps_car_11_cat', 'ps_car_11', 'ps_car_12', 'ps_car_13', 'ps_car_14', 'ps_car_15', 'ps_calc_01', 'ps_calc_02', 'ps_calc_03', 'ps_calc_04', 'ps_calc_05', 'ps_calc_06', 'ps_calc_07', 'ps_calc_08', 'ps_calc_09', 'ps_calc_10', 'ps_calc_11', 'ps_calc_12', 'ps_calc_13', 'ps_calc_14', 'ps_calc_16_bin', 'ps_calc_17_bin', 'ps_calc_18_bin', 'ps_calc_19_bin']
+
+    #params = {'learning_rate': 0.01,'num_iterations':5000, 'max_depth': -1, 'objective': 'binary', 'metric': 'auc', 'max_bin': 1000,'is_training_metric': False}
+    params = {
+        'objective':'binary:logistic',        
+        'max_depth':5,
+        'learning_rate':0.07,
+        'eval_metric':'auc',
+        'min_child_weight':6,
+        'subsample':0.8,
+        'colsample_bytree':0.8,
+        'reg_lambda':1.3,
+        'reg_alpha':8,
+        'gamma':10,
+        'scale_pos_weight':1.6
+        #'n_thread':-1
+    }
+    y_pred=[0]*test_df.shape[0]
+    X_pred=test_df[feature_columns_to_use]
     
-def k_averaged_xgb_boost(k, train, test):
-    feature_columns_to_use = ['ps_ind_01','ps_ind_02_cat','ps_ind_03','ps_ind_04_cat','ps_ind_05_cat','ps_ind_06_bin','ps_ind_07_bin','ps_ind_08_bin','ps_ind_09_bin','ps_ind_10_bin','ps_ind_11_bin','ps_ind_12_bin','ps_ind_13_bin','ps_ind_14','ps_ind_15','ps_ind_16_bin','ps_ind_17_bin','ps_ind_18_bin','ps_reg_01','ps_reg_02','ps_reg_03','ps_car_01_cat','ps_car_02_cat','ps_car_03_cat','ps_car_04_cat','ps_car_05_cat','ps_car_06_cat','ps_car_07_cat','ps_car_08_cat','ps_car_09_cat','ps_car_10_cat','ps_car_11_cat','ps_car_11','ps_car_12','ps_car_13','ps_car_14','ps_car_15','ps_calc_01','ps_calc_02','ps_calc_03','ps_calc_04','ps_calc_05','ps_calc_06','ps_calc_07','ps_calc_08','ps_calc_09','ps_calc_10','ps_calc_11','ps_calc_12','ps_calc_13','ps_calc_14','ps_calc_15_bin','ps_calc_16_bin','ps_calc_17_bin','ps_calc_18_bin','ps_calc_19_bin','ps_calc_20_bin']
-    #feature_columns_to_use = ['ps_ind_01','ps_ind_02_cat','ps_ind_03','ps_ind_04_cat','ps_ind_05_cat','ps_ind_06_bin','ps_ind_07_bin','ps_ind_08_bin','ps_ind_09_bin','ps_ind_10_bin','ps_ind_11_bin','ps_ind_12_bin','ps_ind_13_bin','ps_ind_14','ps_ind_15','ps_ind_16_bin','ps_ind_17_bin','ps_ind_18_bin','ps_reg_01','ps_reg_02','ps_reg_03','ps_car_01_cat','ps_car_02_cat','ps_car_03_cat','ps_car_04_cat','ps_car_05_cat','ps_car_06_cat','ps_car_07_cat','ps_car_08_cat','ps_car_09_cat','ps_car_10_cat','ps_car_11_cat','ps_car_11','ps_car_12','ps_car_13','ps_car_14','ps_car_15'] 
-    predictions=[[0,0]]*test.shape[0]
-    for i in range(k):
-        predictions+=predict_xgboost(data, test)
-    return predictions/k
+    for train, test in kf.split(train_df):
+        train_data = train_df.loc[train]
+        test_data = train_df.loc[test]
+        X_train = train_data[feature_columns_to_use]
+        X_test = test_data[feature_columns_to_use]
+        y_train = train_data['target']
+        y_test=test_data['target']
+        xgb_train = xgb.DMatrix(X_train, y_train)
+        xgb_eval = xgb.DMatrix(X_test, y_test)
+        watchlist = [(xgb_train,'train'),(xgb_eval,'test')]  
+        print('Start training...')
+        # train
+        gbm = xgb.train(params,xgb_train,400,watchlist,early_stopping_rounds=50,verbose_eval=50)
+        print('Start predicting...')
+        # predict
+        y_eval = gbm.predict(xgb.DMatrix(X_test))
+        y_pred += gbm.predict(xgb.DMatrix(X_pred))
+        results += gini_xgb(y_eval, test_data)
     
+    return results/k, y_pred/k
+
 def submit(predictions, test_df):
-    submission = pd.DataFrame({ 'id': [int(a) for a in test_df['id']],'target': predictions[:,1] })
+    submission = pd.DataFrame({ 'id': [int(a) for a in test_df['id']],'target': predictions[:] })
     print(submission)
     submission.to_csv("submission.csv", index=False) 
     
