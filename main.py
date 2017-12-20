b622d6728271721307a8269894a5e89ce221c5c6
LearnerDeOuf13 <etienne.pochet@hotmail.fr>
2017-12-20 23:25:46 +0100

    quelques com sur les pred

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from clustering_missing import *
 
 print("preprocessing...")
 data_train = read_dataset('train.csv')
-#data2_train = mean_imputation(data_train)
-
-
-data_train_modified = cluster_missing(data_train)
-
 data_test_ini = read_dataset('test.csv')
+#data2_train_modified = mean_imputation(data_train)
+#data_test_modified = mean_imputation(data_test_ini)
+data_train_modified = cluster_missing(data_train)
 data_test_modified = cluster_missing(data_test_ini)
-#data_test = mean_imputation(read_dataset('test.csv'))
+
 print("processing...")
-s1,y1=test(data_train,data_test_ini)
-s2,y2=test(data_modified,data_modified)
-predict_xgboost(data_train_modified, data_test_modified)
+s1,y1=Xgb_and_Lgb(data_train,data_test_ini)
+#s2,y2=Xgb_and_Lgb(data_modified,data_modified)
+#predict_xgboost_k_fold(data_train,data_test_ini,5)
+#predict_lgboost_k_fold(data_train,data_test_ini,5)
+#predict_xgboost_k_fold(data_modified,data_modified,5)
+#predict_lgboost_k_fold(data_modified,data_modified,5)
--- visualization.py
+++ visualization.py
@@ -8,13 +8,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def corr_heatmap(v):
+def corr_heatmap(data):
     correlations = data.corr()
 
     # Create color map ranging between two colors
     cmap = sns.diverging_palette(220, 10, as_cmap=True)
 
     fig, ax = plt.subplots(figsize=(10,10))
-    sns.heatmap(correlations, cmap=cmap, vmax=1.0, center=0, fmt='.2f',
-                square=True, linewidths=.5, annot=True, cbar_kws={"shrink": .75})
+    sns.heatmap(correlations, cmap=cmap, vmax=1.0, center=0, fmt='.2f',square=True, linewidths=.5, cbar_kws={"shrink": .75})
     plt.show();
